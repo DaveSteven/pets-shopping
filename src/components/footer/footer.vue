9584fb978994b330d84c5edcc6a218f79f59a623
david <dave_dev@163.com>
2019-03-14 19:47:15 +0800

    添加底部组件

--- src/components/footer/footer.vue
+++ src/components/footer/footer.vue
@@ -2,8 +2,69 @@
   <div class="footer-wrapper">
     <div class="container">
       <footer>
-        <div class="footer-head"></div>
-        <div class="footer-body"></div>
+        <div class="footer-head">
+          <div class="share-icon">
+            <span class="circle mr20"><Icon type="moments" /></span>
+            <span class="circle mr20"><Icon type="qzone" /></span>
+            <span class="circle"><Icon type="weibo" /></span>
+          </div>
+          <div class="pay-icon">
+            <Icon class="mr20" type="alipay" />
+            <Icon class="mr20" type="wechat-pay" />
+            <Icon type="visa" />
+          </div>
+        </div>
+        <div class="footer-body">
+          <div class="shortcut">
+            <div class="item">
+              <h5 class="title">关于</h5>
+              <ul>
+                <li>公司简介</li>
+                <li>新闻动态</li>
+                <li>网站地图</li>
+              </ul>
+            </div>
+            <div class="item">
+              <h5 class="title">教育</h5>
+              <ul>
+                <li>食物中心</li>
+                <li>呵护指导</li>
+                <li>宠物中心</li>
+                <li>小动物中心</li>
+                <li>鸟类中心</li>
+              </ul>
+            </div>
+            <div class="item">
+              <h5 class="title">服务</h5>
+              <ul>
+                <li>宠物营地</li>
+                <li>宠物旅馆</li>
+                <li>宠物美容</li>
+                <li>狗狗训练</li>
+                <li>宠物交流</li>
+              </ul>
+            </div>
+            <div class="item">
+              <h5 class="title">流行</h5>
+              <ul>
+                <li>狗狗&猫咪</li>
+                <li>爬虫动物</li>
+                <li>鸟&鱼</li>
+                <li>小动物</li>
+                <li>打折销售</li>
+              </ul>
+            </div>
+            <div class="item">
+              <h5 class="title">订阅</h5>
+              <p>订阅我们的网站，我们会每周发布动态给您。</p>
+              <div class="mt10 mb15"><input class="footer-input" type="text"></div>
+              <p><a href="javascript:;">提交<Icon type="right-arrow" :size="12"></Icon></a></p>
+            </div>
+          </div>
+          <div class="copy-right">
+            <span>&copy; 2019 萌宠家园</span> All rights reserved.
+          </div>
+        </div>
       </footer>
     </div>
   </div>
