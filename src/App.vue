f47c2fb4fa2b984e15c9626eb87dd684edbd0a01
david <dave_dev@163.com>
2019-03-23 17:28:43 +0800

    修改代码

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
   <div id="app">
     <Header></Header>
     <div class="container">
-      <MiniNav v-if="!isHomePage"></MiniNav>
       <router-view />
     </div>
     <Contacts class="mt50"></Contacts>
@@ -16,7 +15,6 @@ import Header from '_c/header'
 import Contacts from '_c/contacts'
 import Footer from '_c/footer'
 import LoginModal from '_c/login-modal'
-import MiniNav from '_c/mini-nav'
 
 export default {
   name: 'app',
@@ -24,13 +22,7 @@ export default {
     Header,
     Contacts,
     Footer,
-    LoginModal,
-    MiniNav
-  },
-  computed: {
-    isHomePage () {
-      return this.$route.path === '/'
-    }
+    LoginModal
   }
 }
 </script>
