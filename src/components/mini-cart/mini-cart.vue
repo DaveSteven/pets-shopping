e69e527b98d5e889abb40ddab81cd37109a2477e
david <dave_dev@163.com>
2019-03-23 13:58:43 +0800

    解决购物车在商品多的情况下无法滚动的问题

--- src/components/mini-cart/mini-cart.vue
+++ src/components/mini-cart/mini-cart.vue
@@ -1,37 +1,35 @@
 <template>
-  <div class="cart-wrapper">
+  <div class="mini-cart">
     <Dropdown placement="bottom-end" trigger="click" @on-visible-change="visibleChange">
-      <div class="cart-btn">
+      <div class="mini-cart-btn">
         <span><Icon type="cart" :size="14" />购物车</span>
         <Icon type="down-arrow" :size="12" />
       </div>
-      <DropdownMenu slot="list">
-        <div class="cart-container">
-          <template v-if="cartList.length">
-            <Scroll class="cart-list-wrapper" :data="cartList" ref="listview">
-              <ul class="cart-list">
-                <li class="cart-item" v-for="(item, index) in cartList" :key="index">
-                  <div class="img"><img :src="item.img"></div>
-                  <div class="text">
-                    <h5 class="title">{{ item.name }}</h5>
-                    <div class="price">
-                      {{ item.count }} x <span class="text-primary f16">¥{{ item.price | decimal }}</span>
-                    </div>
+      <DropdownMenu class="mini-cart-drop_list" ref="drop" slot="list">
+        <template v-if="cartList.length">
+          <Scroll ref="scroll" class="cart-list-wrapper" :data="cartList">
+            <ul class="cart-list">
+              <li class="cart-item" v-for="(item, index) in cartList" :key="index">
+                <div class="img"><img @load="loadImage" :src="item.img"></div>
+                <div class="text">
+                  <h5 class="title">{{ item.name }}</h5>
+                  <div class="price">
+                    {{ item.count }} x <span class="text-primary f16">¥{{ item.price | decimal }}</span>
                   </div>
-                </li>
-              </ul>
-            </Scroll>
-            <div class="text-right text-bold pr20 mb10 f14">总计：¥{{ total }}</div>
-            <Row type="flex" justify="space-between">
-              <Col><Button type="default" @click="viewDetail">查看详情</Button></Col>
-              <Col><Button type="primary" @click="checkout">结算</Button></Col>
-            </Row>
-          </template>
-          <div v-else class="text-primary text-bold">购物车是空的～</div>
-        </div>
+                </div>
+              </li>
+            </ul>
+          </Scroll>
+          <div class="text-right text-bold pr20 mb10 f14">总计：¥{{ total }}</div>
+          <Row type="flex" justify="space-between">
+            <Col><Button type="default" @click="viewDetail">查看详情</Button></Col>
+            <Col><Button type="primary" @click="checkout">结算</Button></Col>
+          </Row>
+        </template>
+        <div v-else class="text-primary text-bold">购物车是空的～</div>
       </DropdownMenu>
     </Dropdown>
-    <div class="cart-num">{{ totalCount }}</div>
+    <div class="mini-cart-num">{{ totalCount }}</div>
   </div>
 </template>
 <script>
@@ -61,6 +59,11 @@ export default {
       return this.cartList.length
     }
   },
+  data () {
+    return {
+      checkLoaded: false
+    }
+  },
   methods: {
     viewDetail () {
       this.$router.push({
@@ -71,10 +74,19 @@ export default {
     visibleChange (visible) {
       if (visible) {
         this.addScrollEffect()
+        // 每次打开之后要重新计算scroll，不然不会滚动
+        this.$nextTick(() => {
+          this.$refs.scroll.refresh()
+        })
       } else {
         this.removeScrollEffect()
       }
-      this.$refs.listview.refresh()
+    },
+    loadImage () {
+      if (!this.checkLoaded) {
+        this.$refs.scroll.refresh()
+        this.checkLoaded = true
+      }
     }
   }
 }
