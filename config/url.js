c3e9d1224359fbd9906838fde25f75be8dc5f088
david <dave_dev@163.com>
2019-04-14 20:26:50 +0800

    修改生产接口地址

--- config/url.js
+++ config/url.js
@@ -1,6 +1,6 @@
 import env from './env'
 
 const DEV_URL = 'http://localhost:9997'
-const PRO_URL = 'https://produce.com'
+const PRO_URL = 'http://47.100.12.81:8080'
 
 export default env === 'development' ? DEV_URL : PRO_URL
